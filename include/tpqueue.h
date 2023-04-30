09b2c35f2401ef69a3020902f09902f741bd4703
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-04-30 23:25:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,44 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-template<typename T>
+#include <list>
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  list<T> list1;
+  T CopyObj;
+  int head;
+ public:
+  TPQueue() :head(-1) { }
+  bool isEmpty() {
+    return head == -1;
+  }
+  T pop():head(-1) {
+    auto i = list1.front();
+    list1.pop_front();
+    return i;
+  }
+  T top() {
+    return list1.front();
+  }
+  void push(T val) {
+    if (head == -1) {
+      list1.push_front(val);
+      ++head;
+    } else {
+      CopyObj = list1.front();
+      if (CopyObj.prior == val.prior) {
+        list1.push_front(val);
+        list.sort([](const SYM &val1, const SYM &val2) {
+          return val1.ch <= val2.ch});
+      } else {
+        list1.push_front(val);
+        list.sort([](const SYM &val1, const SYM &val2) {
+          return val1.ch <= val2.ch});
+      }
+      head++;
+    }
+  }
 };
 
 struct SYM {
